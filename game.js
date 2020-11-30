7a1119875d4eaa94ed296554083df161995266cd
Brian <bybyby@gmail.com>
2020-11-29 17:19:35 -0800

    Refactor playSound to own function

--- game.js
+++ game.js
@@ -2,19 +2,6 @@
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
-// var randomNumber;
-
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-
-  // Button flash animation
-    $("#" + randomChosenColor).fadeOut(40).fadeIn(40);
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
-}
-
 
 // On click listener handler Button flash animation and clicked
 $(".btn").on("click", function() {
@@ -23,8 +10,22 @@ $(".btn").on("click", function() {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
 
-  $("." + userChosenColor).fadeOut(40).fadeIn(40);
-  var audio = new Audio("sounds/" + userChosenColor + ".mp3");
-  audio.play();
+  playSound(userChosenColor);
 
 });
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  // Button flash animation
+    $("#" + randomChosenColor).fadeOut(40).fadeIn(40);
+
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
