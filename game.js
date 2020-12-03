dd84f56a2ad454f7f09e664be3531dab436ba7ef
Brian <bybyby@gmail.com>
2020-12-03 03:42:07 -0800

    Complete game.  Add check for each button press.  Allow restart on loss.

--- game.js
+++ game.js
@@ -5,22 +5,6 @@ var gamePattern = [];
 var level = 0;
 var gameInProgress = false;
 
-// On click listener handler Button flash animation and clicked
-$(".btn").on("click", function() {
-
-  // console.log($(this).attr("id"));
-  var userChosenColor = $(this).attr("id");
-  userClickedPattern.push(userChosenColor);
-
-  playSound(userChosenColor);
-  animatePress(userChosenColor);
-
-  // Check answer when last button for level is pressed
-  if (userClickedPattern.length === level) {
-    checkAnswer(userChosenColor);
-  }
-});
-
 //Check for keyboard key press to start game
 $(document).keydown(function() {
   if (!gameInProgress) {
@@ -32,19 +16,26 @@ $(document).keydown(function() {
   }
 });
 
+// On click listener handler Button flash animation and clicked
+$(".btn").on("click", function() {
+  // console.log($(this).attr("id"));
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  // Check answer for each click
+  checkAnswer();
+
+});
 
 // Check user input
-function checkAnswer(color) {
-  // Right answer
-  if (gamePattern[level - 1] === color) {
-    userClickedPattern.length = 0;
-    //// TODO: pause time delay
-    setTimeout(function () {
-      nextSequence();
-    }, 1000);
+function checkAnswer() {
+  var btnCount = userClickedPattern.length;
 
   // Wrong answer
-  } else {
+  if (userClickedPattern[btnCount-1] != gamePattern[btnCount-1]) {
     var wrongSound = new Audio("sounds/wrong.mp3");
     wrongSound.play();
 
@@ -55,6 +46,19 @@ function checkAnswer(color) {
 
     $("h1").html("Game over, press any key to restart");
 
+    startOver();
+  }
+  // Correct answer
+  else {
+    // If last button of round, go to next round
+    if (userClickedPattern.length === level) {
+      userClickedPattern.length = 0;
+      //// TODO: pause time delay
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
   }
 
 }
@@ -84,4 +88,11 @@ function playSound(name) {
 // Button flash animation
 function animatePress(currentColor) {
     $("#" + currentColor).fadeOut(40).fadeIn(40);
+}
+
+// Reset values after losing game
+function startOver() {
+  gamePattern = [];
+  level = 0;
+  gameInProgress = false;
 }
